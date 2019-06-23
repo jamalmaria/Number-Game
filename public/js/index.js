2563dfe64bc7f5bcf6c112c3b08424e46dab822e
jamalmaria <jamalmaria111@gmail.com>
2019-06-23 22:26:36 +0530

    Hide function

--- public/js/index.js
+++ public/js/index.js
@@ -10,14 +10,14 @@ function check(){
         message.innerHTML = `<p style = 'color: green;'>Hey You Have Guessed It Right!</p>`;
         randomNumber = Math.floor(Math.random() * 11);
        console.log(randomNumber);
-        // hide()
+        hide()
         
     }else if(input > randomNumber){
         document.body.style.background= "none";
         document.body.style.backgroundColor= "red";
         message.innerHTML = `<p style = 'color: red;'>Number is too high.</p>`;
       
-        // hide()
+        hide()
         
     }else if(input < randomNumber){
         message.innerHTML = `<p style = 'color: red;'>Number is too low.</p>`;
@@ -26,12 +26,12 @@ function check(){
     }else{
         message.innerHTML = `<p style = 'color: red;'>Please add something</p>`;
         document.body.style.background= "url('../Medi/1.jpg')";
-        // hide()
+        hide()
     }
-// function hide(){
-//     setInterval(function(){
-//         message.innerHTML = ""
-//     }, 10000)
-// }
+function hide(){
+    setInterval(function(){
+        message.innerHTML = ""
+    }, 10000)
+}
 }
 let button = document.getElementById('changeColor');
